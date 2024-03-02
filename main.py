727fd0c0be3570afe6faead3ac30360b1c19ac4a
Jayraj Rajput <jayrajput1997@gmail.com>
2024-03-02 23:18:08 +0100

    data ingestion pipline , main.py complete

--- main.py
+++ main.py
@@ -1,6 +1,19 @@
 
 
-from src.cnnClassifier  import logger 
+from cnnClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.components.data_ingestion import DataIngestion
+from cnnClassifier import logger
+
+STAGE_NAME = "Data Ingestion stage"
+### Data Ingestion Phase ####
 
 
-logger.info("welcome to  the main function")
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = DataIngestionTrainingPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
