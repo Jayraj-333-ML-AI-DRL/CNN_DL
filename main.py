6b6f1272a961c5caacb588056be44770574399d0
Jayraj Rajput <jayrajput1997@gmail.com>
2024-03-10 21:22:20 +0100

    dvc deleted

--- main.py
+++ main.py
@@ -2,8 +2,11 @@
 
 from cnnClassifier.pipeline.stage_1_data_ingestion import DataIngestionTrainingPipeline
 from cnnClassifier.pipeline.stage_2_preparation_base_model import PrepareBaseModelPipeline
+from cnnClassifier.pipeline.stage_3_model_traning import TrainingModelPipeline
+from cnnClassifier.pipeline.stage_4_data_validation import Evaluationpipline
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier.components.prepare_base_model import PrepareBaseModel
+from cnnClassifier.components.Training_model import  Training
 from cnnClassifier import logger
 
 STAGE_NAME = "Data Ingestion stage"
@@ -33,3 +36,33 @@ if __name__ == '__main__':
     except Exception as e:
         logger.exception(e)
         raise e
+    
+    
+    
+    STAGE_NAME = "Model Training stage"
+    
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = TrainingModelPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+    
+STAGE_NAME = "Model evaluation stage"
+
+
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = Evaluationpipline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e   
